cf0eeab71285d1df295f3ef544f34326a60235d1
dutondeumesh <dutondeumesh@gmail.com>
2017-08-02 11:17:23 +0530

    Update IIterrator.php

--- IIterrator.php
+++ IIterrator.php
@@ -1,1 +1,8 @@
 
+<?php
+$file_id = 123; // or something else you got from your MySQL database
+$slug = md5($secret.$file_id);
+ 
+echo '
+<a href="http://mydomain.com/dowload.php?fid='.$slug.'">PHP download file via MySQL</a>';
+
